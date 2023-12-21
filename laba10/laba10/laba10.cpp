b92036717ef0cb6fee5235759e457c2a42e4950d
Kr0sHkA <pisleginkrut@gmail.com>
2023-12-21 10:58:08 +0700

    laba10
    laba10

---
+++ laba10/laba10/laba10.cpp
@@ -0,0 +1,67 @@
+﻿/*
+1. Дан массив A размером N.
+Найти минимальный элемент из его элементов
+с четными номерами: A2, A4, A6, ...
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int min_arr(int*, const size_t&);
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	string str;
+	cout << "Введите путь к файлу с исходными данными:\n";
+	getline(cin, str);
+	ifstream file;
+	file.open(str);
+
+	if (!file)
+	{
+		cerr << "Ошибка! Файл не открыт\n";
+		return 1;
+	}
+
+	size_t n;
+	file >> n;
+	int* array = new int[n];
+
+	for (size_t i = 0; i < n; i++)
+	{
+		file >> array[i];
+	}
+
+	string retstr;			//return string - путь к файлу,в который запишем ответ
+	cout << "Введите путь к файлу, в который запишем ответ:\n";
+	getline(cin, retstr);
+	ofstream retfile;
+	retfile.open(retstr);
+	if (!retfile)
+	{
+		cerr << "Ошибка! Файл не открыт\n";
+		return 1;
+	}
+	retfile << min_arr(array, n);
+	delete[] array;
+	file.close();
+	retfile.close();
+}
+
+int min_arr(int* array, const size_t& n)
+{
+	int min = array[0];
+	for (size_t i = 2; i < n; i += 2)
+	{
+		int mini = array[i];
+		if (mini < min) {
+			min = mini;
+		}
+	}
+	return min;
+}
